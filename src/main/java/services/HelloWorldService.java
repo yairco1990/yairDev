98c5cf73349caf91dfadc01a1c6c43949d522b59
Yair Cohen <yairco1990@gmail.com>
2015-09-07 12:11:35 +0300

    first commit

---
+++ src/main/java/services/HelloWorldService.java
@@ -0,0 +1,30 @@
+package services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+@Path("/hello")
+public class HelloWorldService {
+
+	@GET
+	@Path("/aa")
+	public String getMsg() {
+
+		String output = "Jersey say : ";
+
+		return output;
+
+	}
+
+	@GET
+	@Path("/bb")
+	public String getOtherMsg(@QueryParam("param")String param) {
+
+		String output = "Jersey say : " + param;
+
+		return output;
+
+	}
+
+}
